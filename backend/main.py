563197279c5943f00e4c0a231858897a3b2b6e36
Thanh Duy Trinh <thanhtrinh501@Thanhs-MacBook-Pro-2.local>
2025-11-05 19:15:49 -0800

    requirement.txt

--- backend/main.py
+++ backend/main.py
@@ -200,7 +200,7 @@ async def join_room(request: Request):
     return { "success": True, "roomId": roomId, "clientId": clientId }
     
     
-
+import os
 if __name__ == "__main__":
-    # Run with: python main.py
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
